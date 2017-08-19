7ab471503581153a017950ddf22037899b452317
lee <leerwilson@gmail.com>
2017-08-19 15:59:43 -0400

    correct file names and stuff

--- myNewApi/test/my_new_api_test.js
+++ myNewApi/test/my_new_api_test.js
@@ -1,5 +1,5 @@
 var assert = require("chai").assert;
-var myNewApi = require("../lib/myNewApi", function() {
+var myNewApi = require("../lib/my_new_api", function() {
   it("exports handleRequest", function() {
     assert.typeOf(myNewApi.handleRequest, "function");
   });
