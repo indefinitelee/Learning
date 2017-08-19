5d79b545c352b2148a6b862a7d88a208395c023d
lee <leerwilson@gmail.com>
2017-08-19 15:32:29 -0400

    change .zip output location

--- myNewApi/index.js
+++ myNewApi/index.js
@@ -1,10 +1,10 @@
 // controller code
-// Our index.js holds what we might deem “controller” code. It contains the handler, which AWS invokes as the main function of the Lambda. We load a config file and our main code and in exports.handler we invoke a function we’re about to write, handleRequest, passing it event (a JS object containing the JSON-decoded request body), config (a JS object loaded from our config file) and a Node-style callback of the form function (err, responseObject).
+/* Our index.js holds what we might deem “controller” code. It contains the handler, which AWS invokes as the main function of the Lambda. We load a config file and our main code and in exports.handler we invoke a function we’re about to write, handleRequest, passing it event (a JS object containing the JSON-decoded request body), config (a JS object loaded from our config file) and a Node-style callback of the form function (err, responseObject). */
 
 var package = require("./package.json");
 var myNewApi = require("./lib/my_new_api.js");
 console.log("loaded" + package.name + ", version " + package.version);
 
 exports.handler = function (event, context) {
-myNewApi.handleRequest(event, context.done);
+  myNewApi.handleRequest(event, context.done);
 }
--- myNewApi/test/my_new_api_test.js
+++ myNewApi/test/my_new_api_test.js
@@ -4,3 +4,8 @@ var myNewApi = require("../lib/myNewApi", function() {
     assert.typeOf(myNewApi.handleRequest, "function");
   });
 });
+
+
+
+// doesn't like brackets in URL
+// curl -X POST -d '{"hi": "mom"}' https://td0dhwlzgl.execute-api.us-east-2.amazonaws.com/prod{"received_as_input":{"hi":"mom"}}
