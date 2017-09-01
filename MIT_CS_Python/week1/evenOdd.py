4125c8874db159be229eac282e28776a90c464a3
lee <leerwilson@gmail.com>
2017-09-01 16:08:44 -0400

    conditional

---
+++ MIT_CS_Python/week1/evenOdd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 15:23:59 2017
+
+@author: admin
+"""
+
+x = int (input ('enter an integer: '))
+if x%2 == 0:
+    print('')
+    print('Even')
+else: 
+    print('')
+    print('odd')
+print('Done with conditional')
