6121c4b6429267828bf59d0dd39b27aba936497e
lee <leerwilson@gmail.com>
2017-09-25 11:16:24 -0400

    first reducer

--- redux/redux-fundamentals/fundamentals.js
+++ redux/redux-fundamentals/fundamentals.js
@@ -14,4 +14,26 @@
 
 // 3. To describe state mutations write a pure function that takes previous state and Action and returns next State -- This is a REDUCER.
 
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    return 0; // If state is undefined, return the initial application state
+  }
 
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state; // In case an action is passed in we don't understand
+  }
+}
+
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+
+expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+
+expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1);
