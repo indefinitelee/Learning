0f8ae37f88134a98a95bd8f65b1fd0846db772ba
lee <leerwilson@gmail.com>
2017-09-25 12:20:00 -0400

    delete all

--- redux/redux-fundamentals/app/app.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Redux from 'redux';
-
-import AppRoot from './components/AppRoot.jsx';
-
-ReactDOM.render(AppRoot, document.getElementById('app-root'));
