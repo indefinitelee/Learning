b9e31b0a1c6fe4bfe0608c7c4429deaa20da3740
lee <leerwilson@gmail.com>
2017-11-06 11:35:59 -0500

    first prop

--- Udemy_Complete_React/Hello_React/public/app.jsx
+++ Udemy_Complete_React/Hello_React/public/app.jsx
@@ -1,14 +1,16 @@
 var Greeter = React.createClass({
 	render: function () {
+		var name = this.props.name
 		return (
 			<div>
-		      <h1>Hello Reacting</h1>
+		      <h1>Hello {name}</h1>
+		      <p>This is from a component</p>
 			</div>
 		);
 	}
 });
 
-    ReactDOM.render(
-    	<Greeter/>,
-      document.getElementById('app')
-      );
+ReactDOM.render(
+	<Greeter name="Lee"/>,
+  document.getElementById('app')
+  );
