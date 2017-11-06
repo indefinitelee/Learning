3fb3b00d0c884980c1919f13ccd655ad935d5f41
lee <leerwilson@gmail.com>
2017-11-06 13:41:13 -0500

    pass name change prop

--- Udemy_Complete_React/Hello_React/public/app.jsx
+++ Udemy_Complete_React/Hello_React/public/app.jsx
@@ -1,3 +1,34 @@
+var GreeterMessage = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<h1>something h1</h1>
+				<p>ptag</p>
+			</div>)
+	}
+});
+
+var GreeterForm = React.createClass({
+	onFormSubmit: function (e) {
+		e.preventDefault();
+
+		var name = this.refs.name.value;
+
+		if (name.length > 0) {
+			this.refs.name.value = "";
+			this.props.onNewName(name);
+		}
+	},
+	render: function () {
+		return (
+			<form>
+		      	<input type="text" ref="name"/>
+		      	<button>Set Name</button>
+		   </form>
+			)
+	}
+});
+
 var Greeter = React.createClass({
 	getDefaultProps: function() {
 		return {
@@ -12,20 +43,10 @@ var Greeter = React.createClass({
 		};
 	},
 
-	onButtonClick: function (e) {
-		e.preventDefault();
-
-		var nameRef = this.refs.name;
-
-		var name = nameRef.value;
-		nameRef.value = "";
-
-		if (typeof name === 'string' && name.length >0) {
+	handleNewName: function (name) {
 		this.setState({
 			name: name
 		});
-
-		}
 
 	},
 
@@ -37,10 +58,8 @@ var Greeter = React.createClass({
 		      <h1>Hello {name}</h1>
 		      <p>{message + "!!!!"}</p>
 
-		      <form onSubmit={this.onButtonClick}>
-		      	<input type="text" ref="name"/>
-		      	<button>Set Name</button>
-		      </form>
+		      <GreeterMessage/>
+		      <GreeterForm onNewName={this.handleNewName}/>
 			</div>
 		);
 	}
