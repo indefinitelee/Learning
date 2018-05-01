e240b4391316a8c2e4d5461848dc6e36f0b1cad1
lee <leerwilson@gmail.com>
2018-05-01 17:23:39 -0400

    vid 8 event loop implementation

--- Udemy_Advanced_Node-Grider/loop.js
+++ Udemy_Advanced_Node-Grider/loop.js
@@ -2,10 +2,21 @@
 
 // node myFile.js
 
+// pretend arrays to keep track of tasks
+const pendingTimers = []
+const pendingOSTasks = []
+const pendingOperations = []
+
+// new timers, tasks, operations are recorded from myFile running
 myfile.runContents(); // once executed we enter node js event loop
 
+// how Node decides if event loop should process next tick
 function shouldContinue() {
+	// Check one: any pending setTimeout, setInterval, or setImmediate? 
+	// Check two: any pending OS tasks, e.g. an http server listening to a port
+	// Check three: any long running operations e.g. a  call to fs to read a file off HD
 
+	return pendingTimers.length || pendingOSTasks.length || pendingOperations.length
 }
 // entire body executes in one 'tick';
 while(shouldContinue()) {
